e74324c94a6bb681fb32f3007c8025579160abb4
Mills <amills001c@cacsvml-16845.cable.comcast.com>
2016-02-29 19:02:23 -0800

    XRE

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # poolio
 
 much like https://github.com/thisandagain/fork-pool
-except simpler implementation and clearer documentation (for now)
+except simpler implementation and clearer documentation
 this lib utilizes a child_process pool, using child_process.fork() like so: 
 
 
 ```javascript
-var cp = require('child_process');
-cp.fork('your_file.js');
+const cp = require('child_process');
+const n = cp.fork('your_file.js');
 ```
 
 as per:
@@ -16,74 +16,220 @@ https://nodejs.org/api/child_process.html
 
 
 
-
 ### Installation
 
 ```bash
-npm install --save poolio
+npm install -S poolio
 ```
 
 #### Basic Use
 
-```javascript
+```js
 
-// Parent process
+     // in the parent process, we require the module and initialize a pool
 
-var Pool = require('poolio');
+        const Pool = require('poolio');
 
-var pool = new Pool({
-    filePath: 'child.js',    //path is relative to root of your project
-    size: 3
-});
+        const pool = new Pool({
+            filePath: 'child.js',    //path is relative to root of your project
+            size: 3
+        });
 
-var msg = {'do that':'do that that that'};
 
-pool.any(msg,function(err,result){
-       if(err){
-             console.error(err);
-        }
-        else{
-             console.log(result);
-       }
-});
+        function rankPostsUsingWorkerProcess(postIds, cb){
+        
+               pool.any({action: 'run', posts: postIds}).then(function resolved(posts) {
+                    cb(null, posts);
+                }, function rejected() {
+                    cb(null, []);              //pro-tip, use the rejected handler instead of the catch block, to prevent double-calling of callback
+                }).catch(function (err) {
+                    log.error(err);
+                });
+         }
+       
 
 ```
 
 ```javascript
 
-// in a child process
+// in a child process - simple example
 
-process.on('message', function (msg) {
+process.on('message', function (data) {   //use the closure, it is better that way
 
-    switch(msg){
-      case: 'whatever'
-         doRun(msg);
-    }
-}
-
-function doRun(msg){
-
-
-   //do some work here...
-   
-   
-   //when you are done, then call the following, which will put the process back in the available pool of workers
-  
-   process.send({
-     msg: 'done',  
-     error: 'if there is an error include it as a string here',
-     result: {}
-   });
-  
-}
     
+    const workId = data.workId;
+    
+    
+    var result;
+    
+    try{
+    
+     result = doSomeIntensiveWork();
+     
+     process.send({
+        msg: 'done/return/to/pool',
+        error: null,
+        workId: workId,
+        result: result
+     });
+     
+    }
+    catch(err){
+        process.send({
+            msg: 'error',
+            error: err.stack,
+            workId: workId,
+            result: null
+         });
+    }
+    
+    
+    
+    function doSomeIntensiveWork(){
+    
+       // ....
+    
+        return 'some-very-special-result';
+    
+    }
+    
+    
+});
+
 
 ```
 
-In the future this lib will support other child_process commands besides fork(), but for now you can only point to a file xyz.js
+### Advanced use
+
+```js
+
+     // in the parent process, we require the module and initialize a pool
+
+        const Pool = require('poolio');
+
+        const pool = new Pool({
+            filePath: 'child.js',    //path is relative to root of your project
+            size: 5
+        });
 
 
+        function rankPostsUsingWorkerProcess(postIds, cb){
+        
+               pool.any({action: 'run', posts: postIds}).then(function resolved(posts) {
+                    cb(null, posts);
+                }, function rejected() {
+                    cb(null, []);              //pro-tip, use the rejected handler instead of the catch block, to prevent double-calling of callback
+                }).catch(function (err) {
+                    log.error(err);
+                });
+         }
+       
 
-this lib was sponsored by Coolio's Fantastic Voyage:
+```
+
+```javascript
+
+// in a child process - advanced example
+
+
+const domain = require('domain');
+
+
+process.on('message', function (data) {   //use the closure, it is better that way
+
+    const workId = data.workId;
+    const d = domain.create();
+    
+    d.on('error', _.once(function(err){
+       this.exit();
+       process.send({
+         msg: 'error',
+         error: err.stack,
+         workId: workId,
+         result: null
+       });
+    
+    }));
+    
+    d.run(function(){
+    
+    const actions = [];
+    
+    switch(data.action){
+    
+         case 'foo':
+           actions.push(foo);
+           break;
+         case 'bar':
+           actions.push(bar);
+           break;
+         case 'baz';
+           actions.push(baz);
+           break;
+         case 'all':
+           actions.push(foo);
+           actions.push(bar);
+           actions.push(baz);
+           break;
+         default:
+           throw new Error('No case matched');
+    
+    }
+    
+    
+    Promise.all(actions).then(function(result){
+       
+        process.send({
+           msg: 'done/return/to/pool',
+           result: result,
+           workId: workId
+           error: null
+        });
+    
+    });
+    
+    
+    });
+    
+    function foo(){
+       
+       return new Promise(function(resolve,reject){
+       
+            // ....do some async work...
+        
+       }):
+       
+    }
+    
+    
+    function bar(){
+    
+        return new Promise(function(resolve,reject){
+             
+            // ....do some async work...
+              
+        }):
+    
+    }
+    
+    function baz(){
+    
+       return new Promise(function(resolve,reject){
+              
+           // ....do some async work...
+               
+       }):
+    }
+    
+    
+});
+
+
+```
+
+In the future this lib may support other child_process commands besides fork()
+
+
+this lib was sponsored by Poolio's Fantastic Voyage:
 
 ![alt tag](http://i.ytimg.com/vi/a3QAHZicSjQ/0.jpg)
--- index.js
+++ index.js
@@ -5,18 +5,15 @@
 
 //TODO: https://devnet.jetbrains.com/message/5507221
 //TODO: https://youtrack.jetbrains.com/issue/WEB-1919
-//TODO: after spawning cps, how do we know if they are ready to receive messages and do work?
-
+//TODO: replace underscore with lodash?
 
 /////////////////////////////////////////////////////////////////////////
 
 const isDebug = process.execArgv.indexOf('debug') > 0;
-
-console.log('isDebug:', isDebug);
+if(isDebug) console.log('Poolio isDebug flag set to:', isDebug);
 
 /////////////////////////////////////////////////////////////////////////
 
-//const Promise = require('bluebird');
 const cp = require('child_process');
 const _ = require('underscore');
 const path = require('path');
@@ -29,368 +26,375 @@ const fs = require('fs');
 
 const acceptableConstructorOptions = ['execArgv', 'args', 'size', 'filePath'];
 
-var id = 1; //avoid falsy 0 values
+var id = 1; //avoid falsy 0 values, start with 1
 
 function Pool(options) {
 
-  EE.call(this);
+    EE.call(this);
 
-  this.kill = false;
-  this.all = [];
-  this.available = [];
-  this.msgQueue = [];
-  this.resolutions = {};
-  this.removeNext = false;
-  this.workerIdCounter = 1;
-  this.jobIdCounter = 1;
-  this.okToDelegate = false;
-  this.filePath = null;
-  this.size = null;
-  this.execArgv = null;
-  this.args = null;
+    this.kill = false;
+    this.all = [];
+    this.available = [];
+    this.msgQueue = [];
+    this.resolutions = {};
+    this.removeNext = false;
+    this.workerIdCounter = 1; //avoid falsy 0 values, start with 1
+    this.jobIdCounter = 1; //avoid falsy 0 values, start with 1
+    this.okToDelegate = false;
+    this.filePath = null;
+    this.size = null;
+    this.execArgv = null;
+    this.args = null;
 
 
-  if (typeof options !== 'object') {
-    throw new Error('Options object should be defined for your poolio pool, even if it is an empty object, it is needed');
-  }
+    if (typeof options !== 'object') {
+        throw new Error('Options object should be defined for your poolio pool, even if it is an empty object, it is needed');
+    }
 
-  var opts = _.defaults(_.pick(options, acceptableConstructorOptions), {
-    size: 1,
-    __pool_id: '@poolio_pool_' + id++
-  });
+    const opts = _.defaults(_.pick(options, acceptableConstructorOptions), {
+        size: 1,
+        __pool_id: '@poolio_pool_' + id++
+    });
 
-  if (opts.args && !Array.isArray(opts.args)) {
-    throw new Error('"args" option passed to poolio pool, but args was not an array.');
-  }
+    if(opts.size < 1){
+        throw new Error('Poolio pool size must an integer greater than 0.')
+    }
 
-  if (opts.execArgv && !Array.isArray(opts.execArgv)) {
-    throw new Error('"execArgv" option passed to poolio pool, but execArgv was not an array.');
-  }
+    if (opts.args && !Array.isArray(opts.args)) {
+        throw new Error('"args" option passed to poolio pool, but args was not an array.');
+    }
 
-  this.execArgv = this.execArgv || [];
+    if (opts.execArgv && !Array.isArray(opts.execArgv)) {
+        throw new Error('"execArgv" option passed to poolio pool, but execArgv was not an array.');
+    }
 
-  Object.keys(opts).forEach(key => {
-    this[key] = opts[key];
-  });
+    Object.keys(opts).forEach(key => {
+        this[key] = opts[key];
+    });
 
-  if (this.filePath == null || this.size == null) {
-    throw new Error('need to provide filePath value and size value for Poolio constructor');
-  }
+    this.execArgv = this.execArgv || [];
 
-  if (!path.isAbsolute(this.filePath)) {
-    var root = findRoot(process.cwd());
-    var str = String(this.filePath);
-    this.filePath = path.resolve(root + '/' + str);
-  }
+    if (this.filePath == null || this.size == null) {
+        throw new Error('need to provide filePath value and size value for Poolio constructor');
+    }
 
-  this.on('error', function(err) {
-    console.error(err);
-  });
+    if (!path.isAbsolute(this.filePath)) {
+        const root = findRoot(process.cwd());
+        const str = String(this.filePath);
+        this.filePath = path.resolve(root + '/' + str);
+    }
 
-  for (var i = 0; i < this.size; i++) {
-    this.addWorker();
-  }
+    this.on('error', function (err) {
+        console.error(err);
+    });
 
-  this.okToDelegate = true;
+    for (var i = 0; i < this.size; i++) {
+        this.addWorker();
+    }
+
+    this.okToDelegate = true;
 
 }
 
 util.inherits(Pool, EE);
 
 
-Pool.prototype.addWorker = function() {
+Pool.prototype.addWorker = function () {
 
-  var id = this.workerIdCounter++;
+    const id = this.workerIdCounter++;
 
-  var execArgv = JSON.parse(JSON.stringify(this.execArgv));
+    const execArgv = JSON.parse(JSON.stringify(this.execArgv)); //copy execArgv
 
-  if(isDebug){
-      execArgv.push('--debug=' + (53034 + id)); //http://stackoverflow.com/questions/16840623/how-to-debug-node-js-child-forked-process
-  }
-
-  var n = cp.fork(this.filePath, this.args || [], {
-    detached: true,
-    execArgv: execArgv
-  });
-
-  n.workerId = id;
-
-  n.on('error', (err) => {
-    this.emit('worker-error', err);
-  });
-
-  n.once('exit', () => {
-    this.emit('worker-exited', n);
-  });
-
-
-  this.all.push(n);
-
-  this.emit('worker-created');
-
-  n.on('message', data => {
-    debug('message from worker: ' + data);
-    if (!data.workId) {
-      console.error(' => Poolio => message sent from worker with no workId => ', '\n', JSON.stringify(data));
+    if (isDebug) {
+        execArgv.push('--debug=' + (53034 + id)); //http://stackoverflow.com/questions/16840623/how-to-debug-node-js-child-forked-process
     }
-    switch (data.msg) {
-      case 'done':
-        handleCallback.bind(this)(data);
-        break;
-      case 'return/to/pool':
-        delegateWorker.bind(this)(n);
-        break;
-      case 'done/return/to/pool':
-        handleCallback.bind(this)(data); //probably want to handle callback first
-        delegateWorker.bind(this)(n);
-        break;
-      case 'error':
-        console.error(data);
-        this.emit('error', data); // TODO: handle this error event
-        handleCallback.bind(this)(data);
-        delegateWorker.bind(this)(n);
-        break;
-      case 'fatal':
-        console.error(data);
-        this.emit('error', data); // TODO: handle this error event
-        handleCallback.bind(this)(data);
-        removeSpecificWorker.bind(this)(n);
-        this.addWorker();
-        break;
-      default:
-        console.error('warning: your Poolio worker sent a message that was not recognized.');
-    }
-  });
 
-  if (this.okToDelegate) {
-    //TODO: bug - we should be able to just call delegateCP from here, but there is some problem with that
-    if (this.msgQueue.length > 0) {
-      n.send(this.msgQueue.shift());
+    const n = cp.fork(this.filePath, this.args || [], {
+        detached: true,
+        execArgv: execArgv
+    });
+
+    n.workerId = id;
+
+    n.on('error', (err) => {
+        this.emit('worker-error', err);
+    });
+
+    n.once('exit', () => {
+        this.emit('worker-exited', n);
+    });
+
+
+    this.all.push(n);
+
+    this.emit('worker-created');
+
+    n.on('message', data => {
+        debug('message from worker: ' + data);
+        if (!data.workId) {
+            console.error(' => Poolio => message sent from worker with no workId => ', '\n', JSON.stringify(data));
+        }
+        switch (data.msg) {
+            case 'done':
+                handleCallback.bind(this)(data);
+                break;
+            case 'return/to/pool':
+                delegateWorker.bind(this)(n);
+                break;
+            case 'done/return/to/pool':
+                handleCallback.bind(this)(data); //probably want to handle callback first
+                delegateWorker.bind(this)(n);
+                break;
+            case 'error':
+                console.error(data);
+                this.emit('error', data); // TODO: handle this error event
+                handleCallback.bind(this)(data);
+                delegateWorker.bind(this)(n);
+                break;
+            case 'fatal':
+                console.error(data);
+                this.emit('error', data); // TODO: handle this error event
+                handleCallback.bind(this)(data);
+                removeSpecificWorker.bind(this)(n);
+                this.addWorker();
+                break;
+            default:
+                console.error('warning: your Poolio worker sent a message that was not recognized.');
+        }
+    });
+
+    if (this.okToDelegate) {
+        //TODO: bug - we should be able to just call delegateCP from here, but there is some problem with that
+        if (this.msgQueue.length > 0) {
+            n.send(this.msgQueue.shift());
+        } else {
+            debug('worker is available and is back in the pool');
+            this.available.push(n);
+            debug('pool size for pool ' + this.pool_id + ' is: ' + this.available.length);
+        }
     } else {
-      debug('worker is available and is back in the pool');
-      this.available.push(n);
-      debug('pool size for pool ' + this.pool_id + ' is: ' + this.available.length);
+        this.available.push(n);
     }
-  } else {
-    this.available.push(n);
-  }
 };
 
 
 function removeSpecificWorker(n) {
 
-  if (n) {
-    n.tempId = 'gonna-die';
-    this.available = _.without(this.available, _.findWhere(this.available, {
-      tempId: 'gonna-die'
-    }));
-    this.all = _.without(this.all, _.findWhere(this.all, {
-      tempId: 'gonna-die'
-    }));
-    n.kill();
-  } else {
-    console.error('no worker passed to removeWorker function.');
-  }
+    if (n) {
+        n.tempId = 'gonna-die';
+        this.available = _.without(this.available, _.findWhere(this.available, {
+            tempId: 'gonna-die'
+        }));
+        this.all = _.without(this.all, _.findWhere(this.all, {
+            tempId: 'gonna-die'
+        }));
+        n.kill();
+    } else {
+        console.error('no worker passed to removeWorker function.');
+    }
 }
 
 
-Pool.prototype.removeWorker = function() {
+Pool.prototype.removeWorker = function () {
 
-  var n = this.available.pop();
+    const n = this.available.pop();
 
-  if (n) {
-    n.tempId = 'gonna-die';
-    this.all = _.without(this.all, _.findWhere(this.all, {
-      tempId: 'gonna-die'
-    }));
-    n.kill();
-  } else {
-    this.removeNext = true;
-  }
+    if (n) {
+        n.tempId = 'gonna-die';
+        this.all = _.without(this.all, _.findWhere(this.all, {
+            tempId: 'gonna-die'
+        }));
+        n.kill();
+    } else {
+        this.removeNext = true;
+    }
 
 };
 
 
-Pool.prototype.getCurrentSize = function() {
-  return {
-    available: this.available.length,
-    all: this.all.length
-  }
+Pool.prototype.getCurrentSize = function () {
+    return {
+        available: this.available.length,
+        all: this.all.length
+    }
 };
 
 
 function handleCallback(data) {
 
-  var workId = data.workId;
+    const workId = data.workId;
+    const cbOrPromise = this.resolutions[workId];
 
-  var cbOrPromise = this.resolutions[workId];
+    delete this.resolutions[workId];
 
-  console.log('workId:', workId);
-
-  delete this.resolutions[workId];
-
-  if (cbOrPromise) {
-    if (data.error) {
-      var err = new Error(data.error);
-      if (cbOrPromise.cb) {
-        cbOrPromise.cb(err);
-      } else if (cbOrPromise.reject) {
-        cbOrPromise.reject(err)
-      } else {
-        console.error('this should not happen 1')
-      }
+    if (cbOrPromise) {
+        if (data.error) {
+            var err = new Error(data.error);
+            if (cbOrPromise.cb) {
+                cbOrPromise.cb(err);
+            } else if (cbOrPromise.reject) {
+                cbOrPromise.reject(err)
+            } else {
+                console.error('this should not happen 1')
+            }
+        } else {
+            if (cbOrPromise.cb) {
+                cbOrPromise.cb(null, data.result);
+            } else if (cbOrPromise.resolve) {
+                cbOrPromise.resolve(data.result);
+            } else {
+                console.error('this should not happen 2')
+            }
+        }
     } else {
-      if (cbOrPromise.cb) {
-        cbOrPromise.cb(null, data.result);
-      } else if (cbOrPromise.resolve) {
-        cbOrPromise.resolve(data.result);
-      } else {
-        console.error('this should not happen 2')
-      }
+        console.error('this should not happen 3 - but might if a callback is attempted to be called more than once.')
     }
-  } else {
-    console.error('this should not happen 3 - but might if a callback is attempted to be called more than once.')
-  }
 }
 
 
 function delegateWorker(n) {
 
-  if (this.kill) {
-    try{
-      n.kill();
+    if (this.kill) {
+        try {
+            n.kill();
+        }
+        catch (err) {
+            console.error(err);
+        }
+        return;
     }
-    catch(err){
-      console.error(err);
+
+    if (this.removeNext) {
+        this.removeNext = false;
+        n.tempId = 'gonna-die';
+        this.all = _.without(this.all, _.findWhere(this.all, {
+            tempId: 'gonna-die'
+        }));
+        n.kill();
+        return; //don't push cp back on available queue
     }
-    return;
-  }
 
-  if (this.removeNext) {
-    this.removeNext = false;
-    n.tempId = 'gonna-die';
-    this.all = _.without(this.all, _.findWhere(this.all, {
-      tempId: 'gonna-die'
-    }));
-    n.kill();
-    return; //don't push cp back on available queue
-  }
-
-  if (this.msgQueue.length > 0) {
-    var msg = this.msgQueue.shift();
-    msg.__poolioWorkerId = n.workerId;
-    n.send(msg);
-  } else {
-    debug('worker is available and is back in the pool');
-    this.available.push(n);
-    debug('pool size for pool ' + this.pool_id + ' is: ' + this.available.length);
-  }
+    if (this.msgQueue.length > 0) {
+        const msg = this.msgQueue.shift();
+        msg.__poolioWorkerId = n.workerId;
+        n.send(msg);
+    } else {
+        debug('worker is available and is back in the pool');
+        this.available.push(n);
+        debug('pool size for pool ' + this.pool_id + ' is: ' + this.available.length);
+    }
 }
 
 
-Pool.prototype.any = function(msg, cb) {
+Pool.prototype.any = function (msg, cb) {
 
 
-  if (this.kill) {
-    console.log('warning: pool.any called on pool of dead/dying workers');
-    return;
-  }
-
-  debug('current available pool size for pool_id ' + this.pool_id + ' is: ' + this.available.length);
-
-  var workId = this.jobIdCounter++;
-
-  setImmediate(() => {
-    if (this.available.length > 0) {
-      var n = this.available.shift();
-      n.send({
-        msg: msg,
-        workId: workId,
-        __poolioWorkerId: n.workerId
-      });
-    } else {
-
-      if (this.all.length < 1) {
-        console.log('warning: your Poolio pool has been reduced to size of 0 workers, you will have to add a worker to process new and/or existing messages.');
-      }
-
-      this.msgQueue.push({
-        workId: workId,
-        msg: msg
-      });
+    if (this.kill) {
+        console.log('warning: pool.any called on pool of dead/dying workers');
+        return;
     }
-  });
 
-  if (typeof cb === 'function') {
-    //if(process.domain) process.domain.bind(cb);
-    this.resolutions[workId] = {
-      cb: cb
-    };
-  } else {
-    return new Promise((resolve, reject) => {
-      this.resolutions[workId] = {
-        resolve: resolve,
-        reject: reject
-      };
+    debug('current available pool size for pool_id ' + this.pool_id + ' is: ' + this.available.length);
+
+    const workId = this.jobIdCounter++;
+
+    setImmediate(() => {
+        if (this.available.length > 0) {
+            const n = this.available.shift();
+            n.send({
+                msg: msg,
+                workId: workId,
+                __poolioWorkerId: n.workerId
+            });
+        } else {
+
+            if (this.all.length < 1) {
+                console.log('warning: your Poolio pool has been reduced to size of 0 workers, you will have to add a worker to process new and/or existing messages.');
+            }
+
+            this.msgQueue.push({
+                workId: workId,
+                msg: msg
+            });
+        }
     });
-  }
+
+    const d = process.domain;
+
+    if (typeof cb === 'function') {
+        if (d) d.bind(cb);
+        this.resolutions[workId] = {
+            cb: cb
+        };
+    } else {
+        return new Promise((resolve, reject) => {
+            if (d) {
+                d.bind(resolve);
+                d.bind(reject);
+            }
+            this.resolutions[workId] = {
+                resolve: resolve,
+                reject: reject
+            };
+        });
+    }
 };
 
 
-Pool.prototype.destroy = function() {
+Pool.prototype.destroy = function () {
 
-  // killall and remove all listeners
+    // killall and remove all listeners
 
 
-  return this;
+    return this;
 };
 
 
-Pool.prototype.killAll = function() {
+Pool.prototype.killAll = function () {
 
-  this.kill = true;
-  this.available.forEach(n => {
-    n.kill();
-  });
-
-  return this;
-};
-
-
-Pool.prototype.killAllImmediate = function() {
-
-  this.kill = true;
-  var length = this.all.length;
-  var killed = 0;
-  this.all.forEach(n => {
-    n.once('exit', () => {
-      killed++;
-      if (killed >= length) {
-        this.emit('all-killed', this);
-      }
+    this.kill = true;
+    this.available.forEach(n => {
+        n.kill();
     });
-    n.kill();
-  });
 
-  return this;
+    return this;
+};
+
+
+Pool.prototype.killAllImmediate = function () {
+
+    this.kill = true;
+    const length = this.all.length;
+    var killed = 0;
+    this.all.forEach(n => {
+        n.once('exit', () => {
+            killed++;
+            if (killed >= length) {
+                this.emit('all-killed', this);
+            }
+        });
+        n.kill();
+    });
+
+    return this;
 };
 
 
 function findRoot(pth) {
 
-  var possiblePkgDotJSONPath = path.resolve(path.normalize(String(pth) + '/package.json'));
+    const possiblePkgDotJSONPath = path.resolve(path.normalize(String(pth) + '/package.json'));
 
-  try {
-    fs.statSync(possiblePkgDotJSONPath).isFile();
-    return pth;
-  } catch (err) {
-    var subPath = path.resolve(path.normalize(String(pth) + '/../'));
-    if (subPath === pth) {
-      return null;
-    } else {
-      return findRoot(subPath);
+    try {
+        fs.statSync(possiblePkgDotJSONPath).isFile();
+        return pth;
+    } catch (err) {
+        const subPath = path.resolve(path.normalize(String(pth) + '/../'));
+        if (subPath === pth) {
+            return null;
+        } else {
+            return findRoot(subPath);
+        }
     }
-  }
 }
 
 module.exports = Pool;
