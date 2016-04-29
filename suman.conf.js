9c67e3a9e44e27ebc6a31511f9fa14d6821a51b4
the1mills <alexander.d.mills@gmail.com>
2016-04-28 18:01:37 -0700

    temp before merge

--- suman.conf.js
+++ suman.conf.js
@@ -1,55 +1,56 @@
-/**
- * Created by denman on 2/4/2016.
- */
+//**************************************************************************************************
+// Default Suman config file, should always remain at the root of your project
+// *************************************************************************************************
+
+
+const os = require('os');
+const path = require('path');
 
 
 module.exports = Object.freeze({
 
-    verbose: true, //handles and logs warnings (using warning level?)
-    checkMemoryUsage: true,
-    fullStackTraces: false,
-    uniqueAppName: 'suman',
-    NODE_ENV: 'development',
-    pipeStdOut: 'bunyan',
-    MAX_TOTAL_MEMORY: 3000,
-    browser: 'Firefox',
-    disableAutoOpen: false,
-    expireResultsAfter: '10000000',
-    resultsCapCount: 100,
-    suppressRunnerOutput: true,
-    resultsCapSize: 7000, // 3 gb's
-    output: {
-        'standard': {},
-        'basic': {},
-        'web': {
-            servers: ['localhost']
-        }
+    sumanRunner: {},
+
+    sumanServer: {},
+
+    cmdOpts: {
+        //these can be set by the command line
     },
 
 
-    servers: {
-        'denman-lenovo': {
-            host: '127.0.0.1', //10.172.47.79
-            port: 6969,
-            outputDir: 'C:\\Users\\denman\\suman_results'
-        },
-        'CACSVML-13295.local': {
-            host: '127.0.0.1', //10.172.47.79
-            port: 6969,
-            outputDir: '/Users/amills001c/suman_results'
-        },
-        'jarvissc': {
-            host: '69.252.255.134',
-            port: 6969,
-            outputDir: '/home/amills/suman_results'
-        },
-        'dev85.plaxo.com': {
-            host: '172.20.3.31',
-            port: 6969,
-            outputDir: '/home/amills/suman_results'
+    timeoutToSearchForAvailServer: 2000,
+    sendStderrToSumanErrLogOnly: true,
+    useSuiteNameInTestCaseOutput: false,
+    warningLevel: 3,
+    noFrills: false,
+    defaultTestSuiteTimeout: 15000,
+    maxParallelProcesses: 25,           //maximum parallel processes running at one time
+    ultraSafe: false,                   //if true, Suman reads files before executing any supposed test file and makes sure it's a suman test before running
+    verbose: true,                      //handles and logs warnings (using warning level?)
+    checkMemoryUsage: false,            //limits stack traces to just relevant test case or test line
+    fullStackTraces: false,             //allows you to view more than 3 lines for errors in test cases and hooks
+    uniqueAppName: 'suman',
+    NODE_ENV: 'development',            // NODE_ENV to use if you don't specify one
+    pipeStdOut: 'bunyan',
+    browser: 'Firefox',                 // browser to open test results with
+    disableAutoOpen: false,             // use true if you never want suman to automatically open the browser to the latest test results
+    expireResultsAfter: '10000000',     // test results will be deleted after this amount of time
+    resultsCapCount: 100,               // test results will be deleted if they are 101st oldest run
+    suppressRunnerOutput: true,         // forget what this is for LOL
+    resultsCapSize: 7000, // 3 gb's     // oldest test results will be deleted if the results dir expands beyond this size
 
+
+    reporters: {
+        'tap': 'suman/reporters/tap'
+    },
+
+    servers: {                           // list of servers to output test result data to, with the os.hostname() as the key
+
+        '*default': {
+            host: '127.0.0.1',
+            port: 6969,
+            outputDir: path.resolve(process.env[(process.platform === 'win32' ? 'USERPROFILE' : 'HOME')] + '/suman_results')
         }
     }
-
 
 });
