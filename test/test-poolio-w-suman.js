1f5a161b81268d198c21135e85e146162b6c4ce1
Alexander Mills <amills@cse-1s-dhcp--98-213.eng.vmware.com>
2016-06-08 19:29:43 -0700

    Updated tests

--- test/test-poolio-w-suman.js
+++ test/test-poolio-w-suman.js
@@ -1,11 +1,11 @@
+
+
 const suman = require('suman');
 const Test = suman.init(module, {});
 
 Test.describe('@TestsPoolio', {}, function (assert, path, async) {
 
-
     const Pool = require('../index');
-
 
     const pool = new Pool({
         size: 3,
