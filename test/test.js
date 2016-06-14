d60698379b76427626de358540a5ff37bebc8bbc
Alexander D Mills <Olegzandr@Alexanders-MacBook-Pro.local>
2016-06-13 23:29:02 -0700

    temp

--- test/test.js
+++ test/test.js
@@ -6,8 +6,8 @@
 const suman = require('suman');
 const Test = suman.init(module, {});
 
+//////////
 ////////
-///////
 
 Test.describe('@TestsPoolio', {parallel: true}, function (suite, path, async, assert) {
 
