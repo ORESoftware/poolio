d188190e3d36887138559ab34d11238ba584960f
Alexander Mills <amills@cse-1s-dhcp--98-213.eng.vmware.com>
2016-06-14 11:19:13 -0700

    Last commit before pull from crap mac

--- test/test.js
+++ test/test.js
@@ -6,8 +6,7 @@
 const suman = require('suman');
 const Test = suman.init(module, {});
 
-////////
-///////
+/////
 
 Test.describe('@TestsPoolio', {parallel: true}, function (suite, path, async, assert) {
 
@@ -32,6 +31,7 @@ Test.describe('@TestsPoolio', {parallel: true}, function (suite, path, async, as
 		size: 1,
 		filePath: path.resolve(__dirname + '/fixtures/sample-file.js')
 	});
+
 
 	this.it('tests poolio', t => {
 
