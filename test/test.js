9ea35a23d4330b0e5bf0ca056b14baefbe6aeecb
Alexander Mills <amills@cse-1s-dhcp--98-213.eng.vmware.com>
2016-06-14 19:04:23 -0700

    temp

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ const suman = require('suman');
 const Test = suman.init(module, {});
 
 
-//////
+////////
 
 Test.describe('@TestsPoolio', {parallel: true}, function (suite, path, async, assert) {
 
