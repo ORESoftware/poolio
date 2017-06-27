a1702a671e478dd03595d7a6bf6a9e5192d37225
Olegzandr VD <alex@oresoftware.com>
2017-06-26 18:54:45 -0700

    0.2.54008

--- @push.sh
+++ @push.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ "$BRANCH" == "master" ]]; then
+    echo 'Aborting script because you are not on the right git branch.';
+    exit 1;
+fi
+
 # CM => commit message, default is "set"
 CM=${1:-set}
 
